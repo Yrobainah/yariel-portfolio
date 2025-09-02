ab306a4a7e90fa2184894fae79b4322225a8a7d3
Yrobainah <69661560+Yrobainah@users.noreply.github.com>
2025-09-02 19:15:48 +0200

    activar el anchor scrolling y el snap section (onepage)

--- src/app/features/onepage/onepage.ts
+++ src/app/features/onepage/onepage.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-onepage',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './onepage.html',
-  styleUrl: './onepage.scss'
+  styleUrl: './onepage.scss',
 })
-export class Onepage {
-
-}
+export class Onepage {}
