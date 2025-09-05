300ef4605e153fd6ffe3767d6dbfc5189599360f
Yrobainah <69661560+Yrobainah@users.noreply.github.com>
2025-09-05 15:49:13 +0200

    feat (home) incorporando el home con nuevas actualizaciones

--- src/app/features/onepage/onepage.ts
+++ src/app/features/onepage/onepage.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../core/components/navbar/navbar';
+import { Home } from '../../sections/home/home';
 
 @Component({
   selector: 'app-onepage',
-  imports: [Navbar],
+  imports: [Navbar, Home],
   templateUrl: './onepage.html',
   styleUrl: './onepage.scss',
 })
