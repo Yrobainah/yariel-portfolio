560c5ae8eb381621e4cfa02c7cec03ab80daa0c3
Yrobainah <69661560+Yrobainah@users.noreply.github.com>
2025-09-05 18:41:10 +0200

    feat (contact) configuración del contact.ts e integración con emailjs

--- src/app/sections/contact/contact.ts
+++ src/app/sections/contact/contact.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import emailjs from '@emailjs/browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.scss'
+  styleUrl: './contact.scss',
 })
 export class Contact {
+  name: string = '';
+  email: string = '';
+  message: string = '';
 
+  private readonly serviceId = 'service_t0nz37m';
+  private readonly templateId = 'template_g2yf3e8';
+  private readonly publicKey = 'zVZTyz2-P2bl1Pkr-';
+
+  submitting = false;
+  sentOk: boolean | null = null;
+
+  constructor() {
+    emailjs.init({ publicKey: this.publicKey });
+  }
+
+  async onSubmit(form: NgForm) {
+    if (form.invalid || this.submitting) return;
+
+    this.submitting = true;
+    this.sentOk = null;
+
+    try {
+      await emailjs.send(this.serviceId, this.templateId, {
+        from_name: this.name,
+        from_email: this.email,
+        message: this.message,
+      });
+
+      this.sentOk = true;
+      form.resetForm(); // limpia el formulario
+    } catch (err) {
+      console.error('EmailJS error:', err);
+      this.sentOk = false;
+    } finally {
+      this.submitting = false;
+    }
+  }
 }
