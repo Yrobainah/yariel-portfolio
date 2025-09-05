dcef1b3cf3bf0d0a382a379ff6c8ce1d2944eec8
Yrobainah <69661560+Yrobainah@users.noreply.github.com>
2025-09-05 21:08:28 +0200

    feat (footer) configurando el footer.ts y maquetando el footer

--- src/app/core/components/footer/footer.ts
+++ src/app/core/components/footer/footer.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {}
+export class Footer {
+  year = new Date().getFullYear();
+}
