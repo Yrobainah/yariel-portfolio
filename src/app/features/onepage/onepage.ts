93ab7ba4543443a8d949779099ff2fa33edc4c86
Yrobainah <69661560+Yrobainah@users.noreply.github.com>
2025-09-05 17:20:20 +0200

    feat (projects) incorporación de la sección de projects a onepage

--- src/app/features/onepage/onepage.ts
+++ src/app/features/onepage/onepage.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from '../../core/components/navbar/navbar';
 import { Home } from '../../sections/home/home';
 import { About } from '../../sections/about/about';
+import { Projects } from '../../sections/projects/projects';
 
 @Component({
   selector: 'app-onepage',
-  imports: [Navbar, Home, About],
+  imports: [Navbar, Home, About, Projects],
   templateUrl: './onepage.html',
   styleUrl: './onepage.scss',
 })
